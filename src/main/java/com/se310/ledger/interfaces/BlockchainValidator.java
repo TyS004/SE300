810574a4dc93342549907f9be9afc4bcbf17b7f2
Ty <tylerstier13@gmail.com>
2025-09-24 16:05:03 -0700

    Refactored

--- src/main/java/com/se310/ledger/interfaces/BlockchainValidator.java
+++ src/main/java/com/se310/ledger/interfaces/BlockchainValidator.java
@@ -1,9 +1,11 @@
 package com.se310.ledger.interfaces;
 
+import com.se310.ledger.LedgerException;
+
 /**
  * Interface for blockchain validation
  * Follows Interface Segregation Principle by focusing only on blockchain validation
  */
 public interface BlockchainValidator {
-    void validate() throws Exception;
+    void validate() throws LedgerException;
 }
